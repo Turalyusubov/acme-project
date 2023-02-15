e06970fcd8562f9cb398ee5f630767471c32aad1
Turalyusubov <tu201906152@code.edu.az>
2023-02-15 19:11:27 +0400

    created contact page

--- src/pages/contactPage/ContactPage.js
+++ src/pages/contactPage/ContactPage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import ContactForm from './contactForm/ContactForm'
+import ContactHeader from './contactHeader/ContactHeader'
 import './contactpage.css'
 
 export default function ContactPage() {
     return (
-        <div>ContactPage</div>
+        <div>
+            <ContactHeader />
+            <ContactForm />
+        </div>
     )
 }
--- src/pages/contactPage/contactUs/ContactUs.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../contactpage.css'
-
-export default function ContactUs() {
-    return (
-        <div>ContactUs</div>
-    )
-}
